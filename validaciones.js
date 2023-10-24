5bdb1a89f528513e1e7d6bbac62c16a94e94de72
JoseDiaz <124999520+Josete1312@users.noreply.github.com>
2023-10-24 19:11:42 +0200

    Update validaciones.js

--- validaciones.js
+++ validaciones.js
@@ -1,41 +1,47 @@
-window.onload = function () {
+window.onload = comienzo;
 
-    function esNif(nif) {
-        nif = nif.trim().toUpperCase();
-        
-        const letraInicio = nif[0];
-        const numeros = nif.substr(1, nif.length - 2);
-        let letraControl = nif[nif.length - 1];
-        
-        const letrasInicio = ['X', 'Y', 'Z', 'L', 'K', 'M'];
-    
-        if (letrasInicio.includes(letraInicio)) {
-            if (letraInicio === 'X') letraInicio = '0';
-            else if (letraInicio === 'Y') letraInicio = '1';
-            else if (letraInicio === 'Z') letraInicio = '2';
-    
-            const nifNumerico = letraInicio + numeros;
-            const letrasControl = 'TRWAGMYFPDCHXBNJZSQVHLCKE';
-            const letraCalculada = letrasControl[parseInt(nifNumerico) % 23];
-    
-            if (letraCalculada === letraControl) {
-                return 1;
-            } else {
-                return 2;
-            }
-        } else if (/^\d{6,8}$/.test(numeros) && parseInt(numeros) >= 100000) {
-            return 3;
-        } else {
-            return 0;
-        }
-    }
-    
-    function validarNif() {
-        const inputNif = document.querySelector('input[name="nif"]');
-        const nif = inputNif.value;
-        const resultado = esNif(nif);
-    
-        console.log('Resultado:', resultado);
-    }
-    
+function comienzo() {
+    const nifInput = document.formulario.cif;
+    const mensajeInput = document.formulario.Mensaje;
+    nifInput.value = "";
+    mensajeInput.value = "";
+    document.formulario.validar.onclick = esNif;
 }
+
+function esNif() {
+    const nifInput = document.formulario.cif;
+    const mensajeInput = document.formulario.Mensaje;
+    const nif = nifInput.value.trim();
+    const letraControl = 'TRWAGMYFPDXBNJZSQVHLCKE';
+
+    // Eliminar espacios y convertir a mayúsculas
+    const cadena = nif.replace(/\s/g, '').toUpperCase();
+
+    // Comprobar si la cadena tiene un formato válido
+    let resultado;
+
+    switch (true) {
+        case /(^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$)|(^[XYZLKMYFPDXBNJZSQVHLCKE][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKE]$)/.test(cadena):
+            let numero;
+            if (/[XYZ]/.test(cadena[0])) {
+                // Sustituir X, Y, Z por 0, 1, 2
+                numero = parseInt(cadena.replace(/[XYZ]/, '012').slice(0, -1));
+            } else {
+                numero = parseInt(cadena.slice(0, -1));
+            }
+            const letraCalculada = letraControl[numero % 23];
+            resultado = letraCalculada === cadena[cadena.length - 1] ? "NIF correcto" : "NIF incorrecto";
+            break;
+
+        case /^\d{6,8}$/.test(cadena) && parseInt(cadena) >= 100000:
+            resultado = "DNI";
+            break;
+
+        default:
+            resultado = "No es NIF ni DNI";
+            break;
+    }
+
+    mensajeInput.value = resultado;
+}
+
